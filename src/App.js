22038196cb2903b75728817a4c38723da124127a
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-02-24 15:45:53 -0300

    Requisito 6 - ok

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
       cardImage: '',
       cardRare: 'normal',
       cardTrunfo: false,
       isSaveButtonDisabled: true,
+      deck: [],
     };
     this.onInputChange = this.onInputChange.bind(this);
-    this.saveButton = this.saveButton.bind(this);
+    this.enableButton = this.enableButton.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
   }
 
   onInputChange({ target }) {
@@ -27,10 +29,37 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
-    }, () => this.saveButton());
+    }, () => this.enableButton());
   }
 
-  saveButton() {
+  onSaveButtonClick() {
+    const { state } = this;
+    const card = {
+      name: state.cardName,
+      description: state.cardDescription,
+      image: state.cardImage,
+      attr1: state.cardAttr1,
+      attr2: state.cardAttr2,
+      attr3: state.cardAttr3,
+      rare: state.cardRare,
+      trunfo: state.cardTrunfo,
+    };
+    this.setState((myDeck) => ({
+      deck: [...myDeck.deck, card],
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
+    }));
+    console.log('passei por aqui');
+  }
+
+  enableButton() {
     const { state } = this;
     const sum = parseInt(state.cardAttr1, 10)
     + parseInt(state.cardAttr2, 10)
@@ -74,6 +103,7 @@ class App extends React.Component {
             cardTrunfo={ state.cardTrunfo }
             isSaveButtonDisabled={ state.isSaveButtonDisabled }
             onInputChange={ this.onInputChange }
+            onSaveButtonClick={ this.onSaveButtonClick }
           />
           <Card
             cardName={ state.cardName }
